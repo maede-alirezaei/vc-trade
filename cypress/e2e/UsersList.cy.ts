7a89e2d3f9a3fe41c4260db2c1aace2d3d6d4877
DESKTOP-RLUVES5\Maedeh Tehrani <maede.alirezaei@gmail.com>
2023-11-27 13:55:50 +0100

    Fix typescript error

--- cypress/e2e/UsersList.cy.ts
+++ cypress/e2e/UsersList.cy.ts
@@ -1,19 +1,16 @@
-
-
 import { getUsers } from './utils/getUser'
 
-
+beforeEach(() => {
+  cy.visit('/')
+  getUsers()
+})
 describe('HomeView component', () => {
   it('should display users ', () => {
-    cy.visit('/')
-    getUsers()
     cy.get('.users-list').should('have.length.greaterThan', 0)
   })
 })
 describe('DisplayUser component', () => {
   it('should display selected user', () => {
-    cy.visit('/')
-    getUsers()
     cy.get('.users-list > :nth-child(2)').click()
     cy.get('.user-display-container').contains('Madhura')
   })
