8a6809797099ebd20d42d8053f95c5573e9dddc6
DESKTOP-RLUVES5\Maedeh Tehrani <maede.alirezaei@gmail.com>
2023-11-27 18:07:19 +0100

    Add test for search and scroll, Change favicon

--- cypress/e2e/UsersList.cy.ts
+++ cypress/e2e/UsersList.cy.ts
@@ -3,14 +3,20 @@ import { getUsers } from './utils/getUser'
 beforeEach(() => {
   cy.visit('/')
   getUsers()
+  cy.wait(2000)
 })
 describe('HomeView component', () => {
-  it('should display users ', () => {
-    cy.get('.users-list').should('have.length.greaterThan', 0)
+  it('Should display users ', () => {
+    cy.get('.user-item').should('have.length', 25)
+  })
+  it('Should display more users by scrolling', () => {
+    cy.get('.users-list').scrollTo('bottom')
+    cy.wait(4000)
+    cy.get('.user-item').should('have.length', 50)
   })
 })
 describe('DisplayUser component', () => {
-  it('should display selected user', () => {
+  it('Should display selected user', () => {
     cy.get('.users-list > :nth-child(2)').click()
     cy.get('.user-display-container').contains('Madhura')
   })
--- cypress/e2e/utils/getUser.ts
+++ cypress/e2e/utils/getUser.ts
@@ -1,5 +1,10 @@
+import { UserApiResponse } from '../../../src/services/users'
+
 export function getUsers() {
-  cy.fixture('users.json').then((json) => {
-    cy.intercept('GET', 'https://randomuser.me/api/*', json)
+  console.log('in test bef')
+
+  cy.fixture('users.json').then((json: UserApiResponse) => {
+    cy.intercept('GET', 'https://randomuser.me/api/?page=0&results=25', json).as('getUserData') 
   })
+  // cy.wait('@getUserData')
 }
