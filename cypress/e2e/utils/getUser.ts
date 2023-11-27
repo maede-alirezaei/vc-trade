fdf8cb877641a45f8d56eed022c87e48bb13ebc9
DESKTOP-RLUVES5\Maedeh Tehrani <maede.alirezaei@gmail.com>
2023-11-27 23:04:15 +0100

    Fix cypress intercept

--- cypress/e2e/utils/getUser.ts
+++ cypress/e2e/utils/getUser.ts
@@ -2,11 +2,7 @@ import { UserApiResponse } from '../../../src/services/users'
 
 export function getUsers() {
   cy.fixture('users.json').then((json: UserApiResponse) => {
-    cy.intercept(
-      'GET',
-      'https://randomuser.me/api/?inc=gender,name,email,id,picture,location&exc=login&page=0&results=25',
-      json
-    ).as('getUserData')
+    cy.intercept('GET', 'https://randomuser.me/api/*', json).as('getUserData')
   })
   cy.wait(3000)
 }
